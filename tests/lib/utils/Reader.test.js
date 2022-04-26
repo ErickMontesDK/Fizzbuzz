2d553da61d30630fe4b789c7015ba40c0db4fde0
Erick Montes <erickmontesdk@gmail.com>
2022-04-26 13:40:43 -0500

    adding Linter

--- tests/lib/utils/Reader.test.js
+++ tests/lib/utils/Reader.test.js
@@ -1,9 +1,9 @@
-const Reader = require('./../../../lib/utils/Reader')
+const Reader = require("./../../../lib/utils/Reader");
 
 describe("Probar funcionamiento del lector de archivos Reader.js",() => {
     test("Verificar que lee el archivo",()=>{
-        const file=Reader.readJsonFile('explorers.json')
+        const file=Reader.readJsonFile("explorers.json");
 
-        expect(file).toBeDefined()
+        expect(file).toBeDefined();
     });
-})
+});
