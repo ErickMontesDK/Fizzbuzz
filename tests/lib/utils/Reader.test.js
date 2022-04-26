c6768430cef49789980c03b2f4fdddba592b674a
Erick Montes <erickmontesdk@gmail.com>
2022-04-26 13:16:39 -0500

    desajustando codigo para tronarlo

--- tests/lib/utils/Reader.test.js
+++ tests/lib/utils/Reader.test.js
@@ -1,7 +1,7 @@
 const Reader = require('./../../../lib/utils/Reader')
 
 describe("Probar funcionamiento del lector de archivos Reader.js",() => {
-    test("Verificar que lee el archivo enviado",()=>{
+    test("Verificar que lee el archivo",()=>{
         const file=Reader.readJsonFile('explorers.json')
 
         expect(file).toBeDefined()
